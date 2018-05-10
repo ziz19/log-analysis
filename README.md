4d245f73dc6982eb67b791070942d5e8a7993561
Zinan Zhuang <ziz19@pitt.edu>
2018-05-10 10:54:32 -0400

    fix code error, refactor code, and improve readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Log Analysis
 ---
-log_analysis is written in python which connects to a news database and make one query each to answer the following three questions:
+This project sets up a mock PostgreSQL database for a fictional news website. The provided Python script uses the psycopg2 library to query the database and produce a report that answers the following three questions:
 
  What are the most popular three articles of all time?
 
@@ -15,19 +15,19 @@ Python 3.X
 
 'psycopg2' package installed on python3
 
-[download the news database and unzip](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip)
+Links for database: [https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip]
 
 Virtual box and Vagrant installed
 
-[download the VM configuration files and unzip](https://s3.amazonaws.com/video.udacity-data.com/topher/2018/April/5acfbfa3_fsnd-virtual-machine/fsnd-virtual-machine.zip)
+Vagrant configuration file already in the repository
 
 ## Run the Program
 ---
 Make sure you put the `log_analysis.py` and `newsdata.sql` under **FSND-Virtual-Machine/vagrant/**
 
 1. Use `cd` to change directory to the **FSND-Virtual-Machine/vagrant/**
-2. Start up the vagrant virtual machine by running 'vagrant up'
+2. Start up the vagrant virtual machine by running 'vagrant up'. If this is the first time you run vagrant, it will take sometime to download the image.
 3. `vagrant ssh` to log in
-4. `cd /vagrant` to access the shared folder
+4. `cd /vagrant/` to access the shared folder
 5. If this is the first time you run the program, you need to build the database by `psql -d news -f newsdata.sql`
 6. Run the python script by either `python3 reporting_tool.py` or `python reporting_tool.py` depend on your setup
--- reporting_tool.py
+++ reporting_tool.py
@@ -1,43 +1,85 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import psycopg2
 
+DBNAME = 'news'
 
-db = psycopg2.connect("dbname=news")  # connect to the news database
-c = db.cursor()
 
-# Get the most popular three articles of all time
-c.execute('''select title, count(*) as counts from log, articles
-    where path like concat('%',articles.slug,'%')
-    group by title
-    order by counts desc limit 3 ''')  # select the top three frequently visited article paths
-results = c.fetchall()
-print("The top 3 most popular articles:")
-for (title, views) in results:   # for each article path
-    print('"%s" -- %d views' % (title, views))  # print out its parsed article name and views
-print('')
+def db_connect():
+    """
+    Creates and returns a connection to the database defined by DBNAME,
+    as well as a cursor for the database.
+    Returns:
+        db, c - a tuple. The first element is a connection to the database.
+                The second element is a cursor for the database.
+    """
+    db = psycopg2.connect("dbname={}".format(DBNAME))
+    c = db.cursor()
+    return db, c
 
-# Get the most popular article authors of all time
-c. execute('''select name, count(path) as counts from authors, articles, log
-    where authors.id = articles.author
-    and path like concat('%',articles.slug,'%')
-    group by authors.id
-    order by counts desc ''')   # sort author name descendingly by the views of articles
-results = c.fetchall()
-print("The most popular authors:")
-for (name, views) in results:   # for each article path
-    print('"%s" -- %d views' % (name, views))  # print out its parsed article name and views
-print('')
 
-# Get days when more than 1% of requests lead to errors
-c.execute('''select to_char(time, 'DD Mon YYYY') as day,
-    sum(case when status='404 NOT FOUND' then 1 else 0 end),
-    count(time) from log group by day
-    having sum(case when status='404 NOT FOUND' then 1 else 0 end) >= 0.01*count(time)
-    order by day ''')
-results = c.fetchall()
-print('Days that more than 1% of requests are errors:')
-for (date, errors_counts, overall_counts) in results:
-    print('%s -- %.1f %% errors' % (date, 100*errors_counts/overall_counts))
+def execute_query(query):
+    """
+    execute_query takes an SQL query as a parameter.
+    Executes the query and returns the results as a list of tuples.
+    args:
+    query - an SQL query statement to be executed.
 
-db.close()  # close the database
+    returns:
+    A list of tuples containing the results of the query.
+    """
+    db, c = db_connect()
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    return results
+
+
+def print_top_articles():
+    """Prints out the top 3 articles of all time."""
+    query = '''select title, count(*) as views from log, articles
+        where path = concat('/article/',articles.slug)
+        group by title
+        order by views desc limit 3 '''
+    results = execute_query(query)
+    print("The top 3 most popular articles:")
+    for (title, views) in results:
+        print('{} -- {} views'.format(title, views))
+    print('')
+
+
+def print_top_authors():
+    """Prints a list of authors ranked by article views."""
+    query = '''select name, count(path) as views from authors, articles, log
+        where authors.id = articles.author
+        and path = concat('/article/',articles.slug)
+        group by authors.id
+        order by views desc '''
+    results = execute_query(query)
+    print("The most popular authors:")
+    for (name, views) in results:
+        print('{} -- {} views'.format(name, views))
+    print('')
+
+
+def print_errors_over_one():
+    """
+    Prints out the days where
+    more than 1% of logged access requests were errors."""
+    query = '''select to_char(time, 'DD Mon YYYY') as day,
+        round(100*sum(case when status='404 NOT FOUND' then 1 else 0 end)
+            /count(time), 2) as percentage
+        from log group by day
+        having sum(case when status='404 NOT FOUND' then 1 else 0 end)
+            >= 0.01*count(time)
+        order by day '''
+    results = execute_query(query)
+    print('Days that more than 1% of requests are errors:')
+    for (date, percentage) in results:
+        print('{} -- {}% errors'.format(date, percentage))
+
+
+if __name__ == '__main__':
+    print_top_articles()
+    print_top_authors()
+    print_errors_over_one()
